49af3d18e72c61d4471a87afd400004fdb9f23c1
Stephanos <ste.the@outlook.com>
2020-01-27 09:55:22 +0200

    Add initial theming

--- components/CalendarView.js
+++ components/CalendarView.js
@@ -51,6 +51,32 @@ export const CalendarView = ({currentDate}) => {
       disableArrowLeft={true}
       // Disable right arrow. Default = false
       disableArrowRight={true}
+
+      theme={{
+        backgroundColor: '#ffffff',
+        calendarBackground: 'transparent',
+        textSectionTitleColor: '#b6c1cd',
+        selectedDayBackgroundColor: '#00adf5',
+        selectedDayTextColor: '#ffffff',
+        todayTextColor: '#00adf5',
+        dayTextColor: '#2d4150',
+        textDisabledColor: '#d9e1e8',
+        dotColor: '#00adf5',
+        selectedDotColor: '#ffffff',
+        arrowColor: 'orange',
+        disabledArrowColor: '#d9e1e8',
+        monthTextColor: 'blue',
+        indicatorColor: 'blue',
+        // textDayFontFamily: 'monospace',
+        // textMonthFontFamily: 'monospace',
+        // textDayHeaderFontFamily: 'monospace',
+        textDayFontWeight: '300',
+        textMonthFontWeight: 'bold',
+        textDayHeaderFontWeight: '300',
+        textDayFontSize: 16,
+        textMonthFontSize: 26,
+        textDayHeaderFontSize: 16
+      }}
   />
   )
 }
