41df481844b375da04d558236e12c36ed05c4fe8
Stephanos <ste.the@outlook.com>
2020-01-30 17:40:26 +0200

    trigger google Sign-in instructions

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -8,8 +8,28 @@ import {
   statusCodes,
 } from '@react-native-community/google-signin';
 
+GoogleSignin.configure();
+
 export const AuthLoadingScreen = ({navigation}) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
   const handleLoggedIn = (theIsLoggedIn) => {
     if(isLoggedIn){
@@ -24,7 +44,7 @@ export const AuthLoadingScreen = ({navigation}) => {
   return (
     <ScreenView>
       <Text style={{color:"white", fontWeight:"bold"}}>LOADING SCREEN. Checking if user is logged-in</Text>
-      <Button onPress={() => alert("yo")} title="Login" color={"blue"} />
+      <Button onPress={signIn} title="Login" color={"blue"} />
     </ScreenView>
   )
 }
